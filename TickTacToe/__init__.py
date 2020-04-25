bfa212b60fb91a1bcdf4f7664e020e8181908010
tzahinakash <tzahinakash2000@gmail.com>
2020-04-25 15:57:14 +0300

    improve the table class

--- TickTacToe/__init__.py
+++ TickTacToe/__init__.py
@@ -5,8 +5,6 @@ player1 = Player(raw_input("Player 1 please insert your name: "), 'X')
 player2 = Player(raw_input("Player 2 please insert your name: "), 'O')
 table = Table(player1, player2)
 while not table.is_finish():
-    table.player_move(player2, int(raw_input("Player 2 its your turn please insert row: ")),
-                      int(raw_input("Player 2 its your turn please insert col: ")))
-    table.player_move(player1, int(raw_input("Player 1 its your turn please insert row: ")),
-                      int(raw_input("Player 1 its your turn please insert col: ")))
+    table.player_move(player2)
+    table.player_move(player1)
 
